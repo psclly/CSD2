19cf7e5f081ba61955b7e46ffb13b6bbad445e03
Psclly <psclly@gmail.com>
2023-09-11 15:04:26 +0200

    Playback ritme sequencer klaar!

--- python_basics/playback.py
+++ python_basics/playback.py
@@ -2,41 +2,26 @@
 import simpleaudio as SA
 import time as TIME
 
-#Define a function that takes a filename and amount to play the sound.
-    #Create a wave object (from SA library) with the file inside
-    #Let the user know whats about to happen
-def playAudio(name, amount):
-    audio_object = SA.WaveObject.from_wave_file(name)
-    print('Playing ' + whichAudio + ' ' + str(numPlayBackTimes) + ' times..')
-
-    #For loop with a "range" function. Range returns a number in sequence incrementing by 1 each time. As an argument it takes an integer.
-        #Create an object that will be responsible for playing sound files.
-        #play_object as a variable will automatically become an object that plays a sound due to the .play method from SA.
-        #Start waiting for the play_object object to finish before continuing
-        #After each file, give a print. Add 1 to the index to make sure it doesn't return 0-howManyTimes but 1-howManyTimes
-    for i in range(amount):
-        play_object = audio_object.play()
-        play_object.wait_done()
-        print('Audio File played ' + str(i+1) + ' time(s)!')
-    print('Played everything!')
-
 def enterRhythm(amount):
     wood_audioobject = SA.WaveObject.from_wave_file('wood.wav')
     beats = []
     for i in range(amount):
-        beats[i] = input('Enter the length of note ' + i + 1 + ': ')
+        tempvar_2 = input('Enter the length of note ' + str(i+1) + ': ')
+        print(i)
+        beats.append(float(tempvar_2))
         print(beats[i])
 
 
 
-        
-    bpm = input('Enter a BPM: ')
-    bps = bpm / 60
+    bpm = int(input('Enter a BPM: '))
+    sleepTime = 60 / bpm
     input('Are you ready??')
     TIME.sleep(1)
     for i in range(amount):
-        wood_audioobject.play
-        TIME.sleep(beats[i]*bps)
+        print('Playing..')
+        play = wood_audioobject.play()
+        TIME.sleep(beats[i]*sleepTime)
+        play.stop()
 
     
 
