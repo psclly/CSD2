d3ed851288b05b899d540fe757934c13bdf202d3
Caro <psclly@gmail.com>
2023-12-05 12:59:06 +0100

    Graph with 2 lines

--- Cpp_basics/Opdracht1C_InheritenceSine/src/graph.py
+++ Cpp_basics/Opdracht1C_InheritenceSine/src/graph.py
@@ -7,14 +7,14 @@ print(data.columns)
 # Extract 'time' and 'value' columns
 time = data['Time']  # Replace 'time_column_name' with your time column name
 value = data['Sample']  # Replace 'value_column_name' with your value column name
-
-
+phase = data['Phase']
 
 # Plotting the data
 plt.figure(figsize=(10, 20))  # Adjust figure size if needed
 plt.plot(time, value, linestyle='-', linewidth=2)
+plt.plot(time, phase, linestyle='-', linewidth=2)
 plt.xlabel('Time')
-plt.ylabel('Sample')
+plt.ylabel('Sample/Phase')
 plt.title('Oscillator')
 plt.grid(True)
 plt.show()
--- Cpp_basics/Opdracht1C_InheritenceSine/src/main.cpp
+++ Cpp_basics/Opdracht1C_InheritenceSine/src/main.cpp
@@ -9,18 +9,18 @@
 int main(){
     const float samplerate = 44100;
     Sine wave(20, samplerate);
-    Square wave2(20, samplerate);
+    //Square wave2(20, samplerate);
 
     std::remove("output.csv");
     std::ofstream csvFile("output.csv");
     csvFile << "Time,Sample,Phase" << std::endl;
 
     for(int i = 0; i<=samplerate; i++){
-        wave2.tick();
         wave.tick();
+        //wave2.tick();
         //std::cout << i << " => " << wave.getSample() << std::endl;
         csvFile << i << ", " << wave.getSample() << ", " << wave.getPhase() <<std::endl;
-        csvFile << i << ", " << wave2.getSample() << ", " << wave2.getPhase() <<std::endl;
+        //csvFile << i << ", " << wave2.getSample() << ", " << wave2.getPhase() <<std::endl;
     };
 
     return 0;
